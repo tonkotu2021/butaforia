06b3f6c42ab0a502050e7e8c04117592991c6c3e
tonkotu2021 <93757696+tonkotu2021@users.noreply.github.com>
2021-11-05 17:42:59 +0900

    Add files via upload

---
+++ butaforia/js/butaforia.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $(".slider").slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: true,
+      accessibility:false,
+      arrows:false,
+    });
+  });
